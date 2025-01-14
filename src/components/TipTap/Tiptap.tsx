00c4189aae6c035eda23cfbe61c443281d4535c5
rinaharry <ramandimbisoarinah@gmail.com>
2025-01-14 10:47:39 +0300

    Add underline

--- src/components/TipTap/Tiptap.tsx
+++ src/components/TipTap/Tiptap.tsx
@@ -26,34 +26,24 @@ const extensions = [
 ];
 
 const content = `
-    <h2>
-    Hi there,
-    </h2>
-    <p>
-    this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-    </p>
-    <ul>
-    <li>
-        That’s a bullet list with one …
-    </li>
-    <li>
-        … or two list items.
-    </li>
-    </ul>
-    <p>
-    Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-    </p>
-    <pre><code class="language-css">body {
+<h2>Hi there,</h2>
+<p>this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:</p>
+<ul>
+  <li>
+    <p><em><u>That’s</u></em> a bullet <em><u>list</u></em> with one …</p>
+  </li>
+  <li>
+    <p>… <u>or two list items.</u></p>
+  </li>
+</ul>
+<p>Isn’t that great? And all of that is editable. But wait, there’s more. <strong><em><u>Let’s</u></em></strong> try a code block:</p>
+<pre><code class="language-css">body {
     display: none;
     }</code></pre>
-    <p>
-    I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-    </p>
-    <blockquote>
-    Wow, that’s amazing. Good work, boy! 👏
-    <br />
-    — Mom
-    </blockquote>
+<p>I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.</p>
+<blockquote>
+  <p>Wow, that’s amazing. Good work, boy! 👏 <br>— Mom</p>
+</blockquote>
     `;
 
 const Tiptap = () => {
